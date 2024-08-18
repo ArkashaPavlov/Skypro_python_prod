1a8d260340d07348775597edfb2db91967b3907a
Arkasha <arakdiypavlov@gmail.com>
2024-08-18 15:35:28 +0500

    UI and API tests for diplom _v2

--- Diplom/test_UI.py
+++ Diplom/test_UI.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from new_methoods import Employees
+from methoods import Employees
 from selenium.webdriver.common.keys import Keys
 
 import requests
